5011d0c6a74e97e5f2414308a25987ff64603dca
Damian Arturo Garces Paez <guners86@hotmail.com>
2015-10-13 19:34:03 +0000

    Se agrega funcionalidad para nuevos productos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,13 @@ class ProductsController < ApplicationController
     
     def create
         @product = Product.new(product_params)
+        params[:product][:category_ids].each do |category_id|
+            category = Category.find(category_id)
+            @product.categories << category
+        end
+        
+        p @product.categories
+        
         if @product.save
             redirect_to products_path
         else
@@ -20,7 +27,7 @@ class ProductsController < ApplicationController
     private
     
         def product_params
-            params.require(:product).permit(:name, :price)
+            params.require(:product).permit(:name, :price, :category_ids)
         end
     
 end
